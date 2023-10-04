602bda576b488c8ed4ab0a42c6d87c432a0c77b6
Hussain Hadah <hhadah@uh.edu>
2023-10-03 20:54:34 -0500

    create data by state and year, and another over time

--- programs/02-hate-crime-data.r
+++ programs/02-hate-crime-data.r
@@ -34,3 +34,90 @@ all_data <- all_data %>%
     countyfips      = cfips1,
   ) |> 
   arrange(year, state, agency, date_crime)
+
+# create hate crime dataset by
+# county, state, year, and hate crime type
+
+hate_crime_data <- all_data  |> 
+  group_by(state, year, biasmo1) |>
+  summarise(
+    hate_crimes = n()
+  )
+
+average_over_time <- all_data  |> 
+  group_by(year, biasmo1) |>
+  summarise(
+    hate_crimes = n()
+  )
+
+# create a variable for
+# the state's fips code
+ucr_to_fips <- function(ucr_code) {
+    ucr_to_fips_map <- list(
+        '1' = '01',  # Alabama
+        '2' = '04',  # Arizona
+        '3' = '05',  # Arkansas
+        '4' = '06',  # California
+        '5' = '08',  # Colorado
+        '6' = '09',  # Connecticut
+        '7' = '10',  # Delaware
+        '8' = '11',  # District of Columbia
+        '9' = '12',  # Florida
+        '10' = '13', # Georgia
+        '11' = '16', # Idaho
+        '12' = '17', # Illinois
+        '13' = '18', # Indiana
+        '14' = '19', # Iowa
+        '15' = '20', # Kansas
+        '16' = '21', # Kentucky
+        '17' = '22', # Louisiana
+        '18' = '23', # Maine
+        '19' = '24', # Maryland
+        '20' = '25', # Massachusetts
+        '21' = '26', # Michigan
+        '22' = '27', # Minnesota
+        '23' = '28', # Mississippi
+        '24' = '29', # Missouri
+        '25' = '30', # Montana
+        '26' = '31', # Nebraska
+        '27' = '32', # Nevada
+        '28' = '33', # New Hampshire
+        '29' = '34', # New Jersey
+        '30' = '35', # New Mexico
+        '31' = '36', # New York
+        '32' = '37', # North Carolina
+        '33' = '38', # North Dakota
+        '34' = '39', # Ohio
+        '35' = '40', # Oklahoma
+        '36' = '41', # Oregon
+        '37' = '42', # Pennsylvania
+        '38' = '44', # Rhode Island
+        '39' = '45', # South Carolina
+        '40' = '46', # South Dakota
+        '41' = '47', # Tennessee
+        '42' = '48', # Texas
+        '43' = '49', # Utah
+        '44' = '50', # Vermont
+        '45' = '51', # Virginia
+        '46' = '53', # Washington
+        '47' = '54', # West Virginia
+        '48' = '55', # Wisconsin
+        '49' = '56', # Wyoming
+        '50' = '02', # Alaska
+        '51' = '15'  # Hawaii
+    )
+    
+    if (ucr_code %in% names(ucr_to_fips_map)) {
+        return(ucr_to_fips_map[[as.character(ucr_code)]])
+    } else {
+        return(NULL)
+    }
+}
+
+# Apply this function to a dataframe with column named `state`
+hate_crime_data$fstate <- sapply(hate_crime_data$state, ucr_to_fips)
+hate_crime_data <- hate_crime_data |>
+  filter(state < 55)
+# save data
+write_csv(hate_crime_data, file.path(datasets, "hate_crime_data.csv"))
+write_csv(average_over_time, file.path(datasets, "average_over_time.csv"))
